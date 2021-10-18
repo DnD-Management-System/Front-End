56725854c445ddd06f5823fe3abbaa993c9c133f
Charles Bein <charles.bein@revature.net>
2021-10-18 00:26:48 -0400

    Item and spell creation complete, need to keep doing editing core components, and implement api, changing to bypass login

--- DnDManager/src/app/app-routing.module.ts
+++ DnDManager/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from "./components/login/login.component";
 import { CharacterSheetComponent } from "./components/character-sheet/character-sheet.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: LoginComponent
+    component: DashboardComponent
   },
   {
     path: "character",
